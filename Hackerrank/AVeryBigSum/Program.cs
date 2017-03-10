347c3ed965fcdf466e392f76aac57fa5d6335140
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-10 13:07:19 +0700

    Solution for A Very Big Sum

---
+++ Hackerrank/AVeryBigSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+
+        long result = 0;
+        for (int i=0;i<n;i++)
+        {
+            result += Int32.Parse(arr_temp[i]);
+        }
+        Console.WriteLine(result);
+    }
+}
