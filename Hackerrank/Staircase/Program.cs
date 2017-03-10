b0efe4ce011f6ed0dab91cf9bbcbbc4dd8a0eaeb
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-10 14:49:53 +0700

    Solution for Staircase problem

---
+++ Hackerrank/Staircase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());        
+        int pointer = n-1;
+        for (int i=0;i<n;i++)
+        {    
+            for (int j=0;j<n;j++)
+            {
+                if (j<pointer)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+            }
+            pointer--;
+            Console.WriteLine("");
+        } 
+    }
+}
