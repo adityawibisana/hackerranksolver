923210385b26b14381d4cf836c404478bdc02d45
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-10 19:16:55 +0700

    Add solution for Grading Student problem

---
+++ Hackerrank/GradingStudent/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < n; a0++)
+        {
+            int grade = Convert.ToInt32(Console.ReadLine()); 
+            if (grade < 38)
+            {
+                Console.WriteLine(grade);
+            }
+            else
+            {
+                if ((grade+1)%5==0)
+                {
+                    Console.WriteLine(grade + 1);
+                }
+                else if ((grade+2)%5==0)
+                {
+                    Console.WriteLine(grade + 2);
+                }
+                else
+                {
+                    Console.WriteLine(grade);
+                }
+            }
+        }
+    }
+}
