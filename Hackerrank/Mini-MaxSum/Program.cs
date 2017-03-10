1d38a8a4dc6b3f219748ed888365455f2653481a
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-10 16:00:43 +0700

    Solution for Mini- Max Sum

---
+++ Hackerrank/Mini-MaxSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        String[] inputsStr = Console.ReadLine().Split(' ');
+        int[] inputs = new int[5];
+
+        int min = Int32.MaxValue;
+        int max = Int32.MinValue;
+        long total = 0;
+        for (int i=0;i<inputsStr.Length;i++)
+        {
+            inputs[i] = Int32.Parse(inputsStr[i]);
+            if (inputs[i] < min)
+                min = inputs[i];
+            if (inputs[i] > max)
+                max = inputs[i];
+            total += inputs[i];
+        }
+
+        Console.WriteLine((total-max)+" "+(total-min)); 
+    }
+}
