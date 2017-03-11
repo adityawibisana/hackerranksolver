ac39f3ae33b352696d28074c17972c9a3f391203
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-11 14:51:07 +0700

    Solution for Bon Appetit problem

---
+++ Hackerrank/BonAppetit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        String[] inputs1 = Console.ReadLine().Split(' ');
+        int n = Int32.Parse(inputs1[0]);
+        int k = Int32.Parse(inputs1[1]);
+
+        String[] inputs2 = Console.ReadLine().Split(' ');
+        int[] inputs = new int[n];
+
+        for (int i=0;i<n;i++)
+        {
+            inputs[i] = Int32.Parse(inputs2[i]);
+        }
+
+        int charged = Int32.Parse(Console.ReadLine());
+
+        int totalBill = 0;
+        for (int i=0;i<n;i++)
+        {
+            if (i!=k) //don't charge, Anna didn't eat here
+            {
+                totalBill += inputs[i];
+            }
+        }
+
+        int eachBill = totalBill / 2;
+        if (charged>eachBill)
+        {
+            Console.WriteLine(charged - eachBill);
+        }
+        else
+        {
+            Console.WriteLine("Bon Appetit");
+           
+        }
+    }
+}
