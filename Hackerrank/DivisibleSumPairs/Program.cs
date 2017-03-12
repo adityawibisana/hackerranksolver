30811210e694480f4a6af09e238a06a97ffae328
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-03-12 18:31:13 +0700

    Solution for Divisible Sums Pair

---
+++ Hackerrank/DivisibleSumPairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] a_temp = Console.ReadLine().Split(' ');
+
+        int[] a = a_temp.Select(t => Int32.Parse(t)).ToArray();
+
+        int totalPair = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if ((a[i] + a[j]) % k == 0)
+                    totalPair++;
+            }
+        }
+
+        Console.WriteLine(totalPair);
+    } 
+}
